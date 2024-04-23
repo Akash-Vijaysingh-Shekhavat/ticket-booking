99ffade560680050575600c2fbfebf5bdeb33355
Akash Vijaysingh Shekhavat <mycountryakash@gmail.com>
2024-04-24 00:15:28 +0530

    Added the constructor and getter setter methods to access the private member of the entities(Users,train,Ticket),and TrainInfo an TicketInfo are added

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Train {
+
     private String trainId;
     private String trainNo;
     private Date departTime;
@@ -13,4 +14,79 @@ public class Train {
     private List<List<Integer>> seats;
     private Map<String, Time> station;
     private List<String> stations;
+
+    public Train(String trainId, String trainNo, Date departTime, Date arrivalTime, List<List<Integer>> seats, Map<String, Time> station, List<String> stations) {
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.departTime = departTime;
+        this.arrivalTime = arrivalTime;
+        this.seats = seats;
+        this.station = station;
+        this.stations = stations;
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public Date getDepartTime() {
+        return departTime;
+    }
+
+    public void setDepartTime(Date departTime) {
+        this.departTime = departTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public Map<String, Time> getStation() {
+        return station;
+    }
+
+    public void setStation(Map<String, Time> station) {
+        this.station = station;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainId, trainNo);
+    }
+
+
+
+
+
 }
--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -17,6 +17,49 @@ public class User {
         this.userId = userId;
     }
 
+    public User(){}
+
+    public String getName(){
+        return name;
+
+    }
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
+    public void setHashedPassword(String hashedPassword){
+        this.hashedPassword = hashedPassword;
+    }
+
+
+    public String getUserId(){
+        return userId;
+    }
+    public void setUserId(){
+        this.userId = userId;
+    }
+
+
+    public void printTickets(){
+        for(int i=0;i<ticketsBooked.size();i++){
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+
 
 
 
